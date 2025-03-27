010ae1f715911cdc3bde2fc69962041bea5c8add
Albert <acruz37@csub.edu>
2025-03-26 21:54:44 -0700

    Revisiting single number problem

--- src/php/singleNumber.php
+++ src/php/singleNumber.php
@@ -1,9 +1,13 @@
 namespace tc2025;
 
 function singleNumber($nums) {
+	// Note that a XOR 0 = a, so initialize to zero
 	$a = 0;
-	$n = count($nums);
+	// PHP has all arrays as hash tables by default, and using [] causes
+	// a hash table lookup. The `foreach` language construct is highly 
+	// optimized and is quicker.
 	foreach ( $nums as $i ) {
+		// a XOR a XOR b = b and so on
 		$a ^= $i;
 	}
 	return $a;
