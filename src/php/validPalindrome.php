d89f31248104fea25ffe4c2e595694ca5961080c
Albert <acruz37@csub.edu>
2025-03-30 22:57:55 -0700

    test cases on leetcode also potentially treat 0-9 as a number that may contradict the string being a palindrome. regular expression needs to retain numerics

--- src/php/validPalindrome.php
+++ src/php/validPalindrome.php
@@ -2,7 +2,7 @@ namespace tc2025;
 
 function isPalindrome($s) {
 	// Strip non-alphabetic characters using a regular expression
-	$s = preg_replace("/[^a-zA-Z]/", "", $s);
+	$s = preg_replace("/[^a-zA-Z0-9]/", "", $s);
 	// Exerything to lower case
 	$s = strtolower($s);
 
